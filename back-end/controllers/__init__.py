c0a3d98f8afbdb0d0c4812308d8790b9398e2847
ndtuananh04 <125666050+ndtuananh04@users.noreply.github.com>
2024-11-16 22:35:09 +0700

    delete old back-end

--- back-end/controllers/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_jwt_extended import JWTManager
-jwt_manager = JWTManager()
-
-
-def init_app(app):
-    jwt_manager.init_app(app)
--- back-end/services/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask_mail import Mail
-my_mail = Mail()
-
-
-def init_app(app):
-    my_mail.init_app(app)
