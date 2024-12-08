eabd390f858254e09f3b82066dfd18a50fa66305
lamhoang195 <bahoang19052004@gmail.com>
2024-12-08 11:52:51 +0700

    Update login signup

--- front-end/src/routes/user/(account)/signup/+page.svelte
+++ front-end/src/routes/user/(account)/signup/+page.svelte
@@ -14,7 +14,7 @@
 	let phone_number = '';
 	let email = '';
 	let password = '';
-	
+
 	let errorMessages = {
 		family_name: '',
 		given_name: '',
@@ -111,9 +111,9 @@
 	};
 </script>
 
-<div class="signup">
-	<div class="container">
-		<div class="signup__content">
+<div class="formbold-main-wrapper">
+	<div class="formbold-form-container">
+		<div class="formbold-form-wrapper">
 			<form class="signup__form" on:submit={handleSubmit}>
 				<div class="form-header">
 					<img
@@ -122,14 +122,17 @@
 						class="logo"
 						style="width: 120px; height: auto"
 					/>
-					<h2>Bạn đã sẵn sàng đăng ký tài khoản tại QAirline? Hãy bắt đầu ngay!</h2>
-					<p>Vui lòng điền đầy đủ thông tin cá nhân giống trên CMND/CCCD của bạn.</p>
+					<h2>Đăng Ký QAirline</h2>
+					<p>Vui lòng điền đầy đủ thông tin!</p>
 				</div>
+				
+				<hr class="separator" />
+
 				<fieldset class="personal-info">
 					<legend>Thông tin cá nhân</legend>
-					<div class="signup__group">
-						<div class="form-group">
-							<label for="family_name">Họ</label>
+					<div class="formbold-input-flex">
+						<div>
+							<label for="family_name" class="formbold-form-label"> Họ </label>
 							<input
 								type="text"
 								id="family_name"
@@ -137,7 +140,7 @@
 								placeholder="Ví dụ: NGUYEN"
 								bind:value={family_name}
 								on:input={(e) => handleTextInput('family_name', e)}
-								class={errorMessages.family_name ? 'input-error' : ''}
+								class="formbold-form-input {errorMessages.family_name ? 'input-error' : ''}"
 								required
 							/>
 							{#if errorMessages.family_name}
@@ -147,8 +150,8 @@
 								</div>
 							{/if}
 						</div>
-						<div class="form-group">
-							<label for="given-name">Tên đệm & Tên</label>
+						<div>
+							<label for="given-name" class="formbold-form-label"> Tên </label>
 							<input
 								type="text"
 								id="given-name"
@@ -156,7 +159,7 @@
 								placeholder="Ví dụ: VAN A"
 								bind:value={given_name}
 								on:input={(e) => handleTextInput('given_name', e)}
-								class={errorMessages.given_name ? 'input-error' : ''}
+								class="formbold-form-input {errorMessages.given_name ? 'input-error' : ''}"
 								required
 							/>
 							{#if errorMessages.given_name}
@@ -168,21 +171,21 @@
 						</div>
 					</div>
 
-					<div class="signup__group">
-						<div class="form-group">
-							<label for="date_of_birth">Ngày, Tháng, Năm Sinh</label>
+					<div class="formbold-input-flex">
+						<div>
+							<label for="date_of_birth" class="formbold-form-label"> Ngày, Tháng, Năm Sinh </label>
 							<input
 								type="date"
 								id="date_of_birth"
 								bind:value={date_of_birth}
 								name="date_of_birth"
+								class="formbold-form-input"
 								required
 							/>
 						</div>
-
-						<div class="form-group">
-							<label for="gender">Giới tính</label>
-							<select id="gender" name="gender" bind:value={gender}>
+						<div>
+							<label for="gender" class="formbold-form-label"> Giới Tính </label>
+							<select id="gender" name="gender" bind:value={gender} class="formbold-form-input">
 								<option value="" disabled selected hidden>Giới tính</option>
 								<option value="male">Nam</option>
 								<option value="female">Nữ</option>
@@ -191,150 +194,142 @@
 						</div>
 					</div>
 
-					<div class="form-group">
-						<label for="nationality">Quốc tịch</label>
-						<input
-							type="text"
-							id="nationality"
-							name="nationality"
-							placeholder="Quốc tịch"
-							bind:value={nationality}
-							on:input={(e) => handleTextInput('nationality', e)}
-							class={errorMessages.nationality ? 'input-error' : ''}
-							required
-						/>
-						{#if errorMessages.nationality}
-							<div class="error-message">
-								<span class="error-icon">⚠️</span>
-								<span>{errorMessages.nationality}</span>
-							</div>
-						{/if}
+					<div class="formbold-mb-3">
+						<div>
+							<label for="nationality" class="formbold-form-label"> Quốc Tịch </label>
+							<input
+								type="text"
+								id="nationality"
+								name="nationality"
+								placeholder="Quốc tịch"
+								bind:value={nationality}
+								on:input={(e) => handleTextInput('nationality', e)}
+								class="formbold-form-input {errorMessages.nationality ? 'input-error' : ''}"
+								required
+							/>
+							{#if errorMessages.nationality}
+								<div class="error-message">
+									<span class="error-icon">⚠️</span>
+									<span>{errorMessages.nationality}</span>
+								</div>
+							{/if}
+						</div>
 					</div>
 
-					<div class="form-group">
-						<label for="identification">CMND/CCCD</label>
-						<input
-							type="text"
-							id="identification"
-							bind:value={identification}
-							on:input={(e) => handleInput('identification', e)}
-							placeholder="CMND/CCCD"
-							class={errorMessages.identification ? 'input-error' : ''}
-							required
-						/>
-						{#if errorMessages.identification}
-							<div class="error-message">
-								<span class="error-icon">⚠️</span>
-								<span>{errorMessages.identification}</span>
-							</div>
-						{/if}
+					<div class="formbold-mb-3">
+						<div>
+							<label for="identification" class="formbold-form-label"> CMND/CCCD </label>
+							<input
+								type="text"
+								id="identification"
+								bind:value={identification}
+								on:input={(e) => handleInput('identification', e)}
+								placeholder="CMND/CCCD"
+								class="formbold-form-input {errorMessages.identification ? 'input-error' : ''}"
+								required
+							/>
+							{#if errorMessages.identification}
+								<div class="error-message">
+									<span class="error-icon">⚠️</span>
+									<span>{errorMessages.identification}</span>
+								</div>
+							{/if}
+						</div>
 					</div>
 				</fieldset>
 
+				<hr class="separator" />
+
 				<fieldset class="contact-info">
 					<legend>Thông tin liên hệ</legend>
-					<div class="form-group">
-						<label for="email">Email</label>
-						<input
-							type="email"
-							id="email"
-							name="email"
-							placeholder="Email của bạn"
-							bind:value={email}
-							on:input={handleEmailInput}
-							class={errorMessages.email ? 'input-error' : ''}
-							required
-						/>
-						{#if errorMessages.email}
-							<div class="error-message">
-								<span class="error-icon">⚠️</span>
-								<span>{errorMessages.email}</span>
-							</div>
-						{/if}
+					<div class="formbold-mb-3">
+						<div>
+							<label for="email" class="formbold-form-label"> Email </label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								placeholder="Email của bạn"
+								bind:value={email}
+								on:input={handleEmailInput}
+								class="formbold-form-input {errorMessages.email ? 'input-error' : ''}"
+								required
+							/>
+							{#if errorMessages.email}
+								<div class="error-message">
+									<span class="error-icon">⚠️</span>
+									<span>{errorMessages.email}</span>
+								</div>
+							{/if}
+						</div>
 					</div>
 
-					<div class="form-group">
-						<label for="phone_number">Số điện thoại</label>
-						<input
-							type="text"
-							id="phone_number"
-							bind:value={phone_number}
-							on:input={(e) => handleInput('phone_number', e)}
-							placeholder="Số điện thoại"
-							class={errorMessages.phone_number ? 'input-error' : ''}
-							required
-						/>
-						{#if errorMessages.phone_number}
-							<div class="error-message">
-								<span class="error-icon">⚠️</span>
-								<span>{errorMessages.phone_number}</span>
-							</div>
-						{/if}
+					<div class="formbold-mb-3">
+						<div>
+							<label for="phone_number" class="formbold-form-label"> Số điện thoại </label>
+							<input
+								type="text"
+								id="phone_number"
+								bind:value={phone_number}
+								on:input={(e) => handleInput('phone_number', e)}
+								placeholder="Số điện thoại"
+								class="formbold-form-input {errorMessages.phone_number ? 'input-error' : ''}"
+								required
+							/>
+							{#if errorMessages.phone_number}
+								<div class="error-message">
+									<span class="error-icon">⚠️</span>
+									<span>{errorMessages.phone_number}</span>
+								</div>
+							{/if}
+						</div>
 					</div>
 
-					<div class="form-group">
-						<label for="password">Mật khẩu</label>
+					<div class="formbold-mb-3">
+						<label for="password" class="formbold-form-label"> Mật khẩu </label>
 						<input
 							type="password"
 							id="password"
 							bind:value={password}
 							name="password"
 							placeholder="Mật khẩu"
+							class="formbold-form-input"
 							required
 						/>
 					</div>
 				</fieldset>
 
-				<div class="form-group checkbox-group">
-					<input type="checkbox" id="terms" name="terms" />
-					<label for="terms"
-						>Tôi đã đọc và đồng ý với các điều khoản và điều kiện của QAirline.</label
-					>
+				<div class="formbold-checkbox-wrapper">
+					<label for="supportCheckbox" class="formbold-checkbox-label">
+					  <div class="formbold-relative">
+						<input
+						  type="checkbox"
+						  id="supportCheckbox"
+						  class="formbold-input-checkbox"
+						/>
+						<div class="formbold-checkbox-inner">
+						  <span class="formbold-opacity-0">
+							<svg
+							  width="11"
+							  height="8"
+							  viewBox="0 0 11 8"
+							  fill="none"
+							  class="formbold-stroke-current"
+							>
+							  <path
+								d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
+								stroke-width="0.4"
+							  ></path>
+							</svg>
+						  </span>
+						</div>
+					  </div>
+					  Tôi đồng ý với điều khoản của QAirline
+					</label>
 				</div>
 
-				<button type="submit" class="submit-btn">Đăng ký</button>
+				<button class="formbold-btn">Đăng Ký</button>
 			</form>
 		</div>
 	</div>
 </div>
-
-<style>
-	.form-group {
-		margin-bottom: 20px;
-	}
-
-	input {
-		width: 100%;
-		padding: 10px;
-		font-size: 1rem;
-		border: 1px solid #ccc;
-		border-radius: 4px;
-		transition: border-color 0.3s ease;
-	}
-
-	input:focus {
-		border-color: #007bff;
-		outline: none;
-	}
-
-	input.input-error {
-		border-color: #dc3545;
-	}
-
-	.error-message {
-		display: flex;
-		align-items: center;
-		margin-top: 5px;
-		color: #dc3545;
-		font-size: 0.9rem;
-		background-color: #ffe4e6;
-		padding: 8px 12px;
-		border-radius: 4px;
-		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-	}
-
-	.error-icon {
-		margin-right: 8px;
-		font-size: 1.2rem;
-	}
-</style>
