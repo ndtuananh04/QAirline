0f140da4682cbc6e54091b4e17404ef3773d3f90
lamhoang195 <bahoang19052004@gmail.com>
2024-12-11 23:49:32 +0700

    admin

--- front-end/src/routes/admin/dashboard/+page.svelte
+++
@@ -1,4 +0,0 @@
-<a href="/admin/manager/accounts">Account Manager</a>
-<a href="/admin/manager/airplanes">Airplane Manager</a>
-<a href="/admin/manager/flights">Flight Manager</a>
-<a href="/admin/manager/tickets">Ticket Manager</a>
